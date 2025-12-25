2a7c9c65765675bcfd10bf23ae78994550c27b7c
Priyanshu <chamolip81@gmail.com>
2025-12-25 22:25:37 +0530

    relax rate limit for dev

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,10 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max requests per IP
-  standardHeaders: true,
-  legacyHeaders: false
+  windowMs: 15 * 60 * 1000,
+  max: 500, // increase from low value
 });
 
 export default limiter;
